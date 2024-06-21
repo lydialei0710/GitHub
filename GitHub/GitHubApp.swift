a30a6c021bdc90ee7a8a2ed387280f66ae30a645
lydialei0710 <lydialei0710@gmail.com>
2024-06-21 13:20:40 -0400

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Lydia Lei on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
